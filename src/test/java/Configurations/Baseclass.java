f86e7dcafa9439f652507c0af149a58fa2ee60b6
Your Name <you@example.com>
2021-09-21 19:21:07 +0530

    aw

--- src/test/java/Configurations/Baseclass.java
+++ src/test/java/Configurations/Baseclass.java
@@ -1,23 +1,9 @@
 package Configurations;
 
-import org.junit.Before;
-import org.openqa.selenium.WebDriver;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.*;
 
 public class Baseclass {
 	
-	public WebDriver driver;
-	public ExtentReports report;
-
-
-
-   @Before
-   public void setSuit() {
-	   
-	   
-	   
-   }
+	
    
    }
